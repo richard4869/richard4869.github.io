3c751d2be2e4146fa3b77bb519b328b015071d95
richard4869 <richardyang4869@gmail.com>
2018-02-14 19:47:46 +0800

    obj

--- background.js
+++ background.js
@@ -113,7 +113,7 @@ function render() {
 	scene.traverse( function( object ) {
 		if ( object.isMesh === true ) {
 			object.rotation.x = timer * 5;
-			object.rotation.y = timer * 2.5;
+			//object.rotation.y = timer * 2.5;
 		}
 	} );
 	renderer.render( scene, camera );
