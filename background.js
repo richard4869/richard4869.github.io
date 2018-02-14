452b77ce8a69155224f276e030926f74de893d86
richard4869 <richardyang4869@gmail.com>
2018-02-14 19:29:29 +0800

    z

--- background.js
+++ background.js
@@ -10,6 +10,7 @@ function init() {
 
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
 	camera.position.y = 40;
+	camera.position.z = 40;
 	scene = new THREE.Scene();
 	var light, object;
 
